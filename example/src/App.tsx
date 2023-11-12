ec1b9aeda9688ce845b7775de58debe7238670a2
Kamal Shah <kkamal714@gmail.com>
2023-11-12 18:20:37 +0530

    [Kamal] | Reinitialize project structure

---
+++ example/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'react-custom-component'
+import 'react-custom-component/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
---
+++ src/CustomButton.js
@@ -0,0 +1,10 @@
+import { Button } from "@mui/material"
+import React from 'react'
+
+export default function CustomButton({ text }) {
+  return (
+    <Button>
+      {text}
+    </Button>
+  )
+}
